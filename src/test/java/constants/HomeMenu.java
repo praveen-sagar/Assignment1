faf9ad15b342537a2a218190cefb322b4b6d33e0
Praveen Sagar <46619260+praveen-sagar@users.noreply.github.com>
2022-04-16 23:18:06 +0530

    Add files via upload

--- src/test/java/constants/HomeMenu.java
+++ src/test/java/constants/HomeMenu.java
@@ -1,1 +1,8 @@
+package constants;
 
+public class HomeMenu {
+	public static final String DISCUSS = "Discuss";
+	public static final String INVENTORY = "Inventory";
+	public static final String MANUFACTURING = "Manufacturing";
+	public static final String PREFIX_TITLE = "Odoo - ";
+}
---
+++ src/test/java/constants/NavSubMenu.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class NavSubMenu {
+	public static final String Products = "Products";
+	public static final String Bills_Of_Materials = "Bills of Materials";
+	public static final String Lots_Seria_Numbers = "Lots/Serial Numbers";
+
+}
