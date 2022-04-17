50fc70b2e2aa590696578721965e0767f984e148
Praveen Sagar <46619260+praveen-sagar@users.noreply.github.com>
2022-04-17 17:44:01 +0530

    Delete HomeMenu.java

--- src/test/java/constants/HomeMenu.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public class HomeMenu {
-	public static final String DISCUSS = "Discuss";
-	public static final String INVENTORY = "Inventory";
-	public static final String MANUFACTURING = "Manufacturing";
-	public static final String PREFIX_TITLE = "Odoo - ";
-}
