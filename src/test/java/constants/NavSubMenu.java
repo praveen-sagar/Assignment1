99ffb357d55be811cf6e9a77be55cce2b70002d6
Praveen Sagar <46619260+praveen-sagar@users.noreply.github.com>
2022-04-17 17:44:26 +0530

    Delete NavSubMenu.java

--- src/test/java/constants/NavSubMenu.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public class NavSubMenu {
-	public static final String Products = "Products";
-	public static final String Bills_Of_Materials = "Bills of Materials";
-	public static final String Lots_Seria_Numbers = "Lots/Serial Numbers";
-
-}
