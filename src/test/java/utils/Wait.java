8ff26037e06a0098daf1c8f3b024fc6678b54904
Praveen Sagar <46619260+praveen-sagar@users.noreply.github.com>
2022-04-17 11:40:45 +0530

    Add files via upload

--- src/test/java/utils/Wait.java
+++ src/test/java/utils/Wait.java
@@ -1,1 +1,19 @@
+package utils;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Wait {
+	static WebDriverWait wait;
+	private static final int waitTime = 10;
+
+	public static WebElement waitForElementToBeClickable(WebDriver driver, WebElement element) {
+		wait = new WebDriverWait(driver, Duration.ofSeconds(waitTime));
+		WebElement webElement = wait.until(ExpectedConditions.elementToBeClickable(element));
+		return webElement;
+	}
+}
