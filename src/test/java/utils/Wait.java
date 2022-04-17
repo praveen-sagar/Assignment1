38b97d4344fdd53873268b574eb18a1636cdcd6d
Praveen Sagar <46619260+praveen-sagar@users.noreply.github.com>
2022-04-17 15:54:28 +0530

    Update Wait.java

--- src/test/java/utils/Wait.java
+++ src/test/java/utils/Wait.java
@@ -7,12 +7,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import managers.FileReaderManager;
+
 public class Wait {
 	static WebDriverWait wait;
-	private static final int waitTime = 10;
 
 	public static WebElement waitForElementToBeClickable(WebDriver driver, WebElement element) {
-		wait = new WebDriverWait(driver, Duration.ofSeconds(waitTime));
+		wait = new WebDriverWait(driver,
+				Duration.ofSeconds(FileReaderManager.getInstance().getConfigReader().getExplicitlyWait()));
 		WebElement webElement = wait.until(ExpectedConditions.elementToBeClickable(element));
 		return webElement;
 	}
